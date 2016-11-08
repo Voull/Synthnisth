5986e81773e72be7a27f9314eacf7d72bc83d929
Renan Rodrigues Gonçalves <renanrodgon@yahoo.com.br>
2016-11-08 16:39:06 -0200

    Movimentos do Personagem
    Foi criada uma cena para o protótipo e foi feito o código de
    movimentação para frente e para trás do personagem

---
+++ Assets/Movimentos.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimentos : MonoBehaviour {
+
+	public KeyCode left = KeyCode.A;//botão pra andar pra direita
+	public KeyCode right = KeyCode.D;//botão pra andar pra esquerda
+	public KeyCode jump = KeyCode.W;//botão pra pular
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		if (Input.GetKey (left)) {//Input para andar para esquerda
+			Vector2 goLeft = new Vector2 (-0.2f, 0.0f);
+			transform.Translate (goLeft);
+		} else if (Input.GetKey (right)) {//input para andar para a direita
+			Vector2 goRight = new Vector2 (0.2f, 0.0f);
+			transform.Translate (goRight);
+		}
+	}
+}
