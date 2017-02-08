fb3c6fab5c9df569b4b8c2bd2acb6d82030b17e9
Renan Rodrigues Gonçalves <renanrodgon@yahoo.com.br>
2017-02-08 16:58:15 -0200

    Comportamentos refatorados
    Os scripts dos comportamentos do personagem foram refatorados, as teclas
    de direções foram trocadas para as setas do teclado

---
+++ Assets/CharacterBehaviours.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterBehaviours : MonoBehaviour {
+
+	public KeyCode left = KeyCode.LeftArrow;//botão pra andar pra esquerda
+	public KeyCode right = KeyCode.RightArrow;//botão pra andar pra direita
+	public KeyCode jump = KeyCode.S;//botão pra pular
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		Walk();
+	}
+
+	void Walk(){
+		if (Input.GetKey (left)) {//Input para andar para esquerda
+			Vector2 goLeft = new Vector2 (-0.2f, 0.0f);
+			transform.Translate (goLeft);
+		} else if (Input.GetKey (right)) {//input para andar para a direita
+			Vector2 goRight = new Vector2 (0.2f, 0.0f);
+			transform.Translate (goRight);
+		}
+	}
+
+	void Jump(){
+		if (Input.GetKey (jump)) {
+			
+		}
+	}
+}
