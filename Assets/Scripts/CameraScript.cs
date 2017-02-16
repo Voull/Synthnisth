0ed4723512cebeba5e405939dafd4debf79b6b28
Renan Rodrigues Gonçalves <renanrodgon@yahoo.com.br>
2017-02-16 21:11:21 -0200

    Câmera concluída
    Agora a câmera se move junto com o personagem

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public Transform pt;
+	public Transform ct;
+	
+	// Update is called once per frame
+	void Update () {
+		ct.position = Vector3.Lerp(
+			ct.position,
+			new Vector3 (pt.position.x, ct.position.y, ct.position.z),
+			1f);
+	}
+}
