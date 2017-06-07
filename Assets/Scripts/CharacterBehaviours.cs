6fa781f0f68f0c1fb136f1f9719bdc570223be86
Arthur Carnieto <arthurcarnieto@gmail.com>
2017-06-07 15:55:05 -0300

    Reparos no pulo duplo
    - Houve alteração do uso ray casting no pulo para colliders
    - Game Over criado
    - Novos prefabs
    - Colisores do chão com o pé e da espada

--- Assets/Scripts/CharacterBehaviours.cs
+++ Assets/Scripts/CharacterBehaviours.cs
@@ -1,67 +1,111 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
+public enum Power {powerUp1, powerUp2, powerUp3};
 public class CharacterBehaviours : MovingCharacter {
 
-	public float jumpForce = 350f;
-	public KeyCode jump = KeyCode.S;//botão pra pular
-	//public int vidas = 1;//por enquanto vidas = 1, pode mudar depois
-	public Vector2 distToGround;
-	public Bounds b;
-	bool candoublejump = false;
+	//public bool striking = false;
+	public int jumps = 0;//valor que controla a quantidade de saltos realizada
+	public float knockBack = 0;//valor que controla o tempo de "atordoamento" após levar dano
+	public BoxCollider2D bc;//passar o character como parâmetro no inspector
+	bool effect = false;
+	int totalLife = 4;
+	int life;
+	float jumpForce = 550f;
+	float invencibilityTime = 0;//valor que controla o tempo de invencibilidade após levar dano
+	KeyCode jump = KeyCode.S;//botão pra pular
+	//KeyCode strike = KeyCode.D;//botão pra atacar
+	//Power effect;
 
 	void Start () {
-		speed = 12f;
-		rb = GetComponent<Rigidbody2D> ();//recebe o componente Ridigbody2D do personagem
-		transform = GetComponent<Transform>();//recebe o componente Transform do personagem 
-
-		b = new Bounds (transform.position, new Vector2(0,-1));
-		distToGround = (Vector2) b.extents;
+		life = totalLife;
+		speed = 20f;
 	}
 
-	void FixedUpdate() {
-		Move(Input.GetAxis ("Horizontal"));
-		Jump ();
-	}
-
-	bool grounded() {
-		return Physics2D.Raycast (transform.position, -Vector3.up, 1f);
-	}
-
-	void Jump() {
-		if (Input.GetKeyDown (jump)) {
-			if (grounded()) {
-				rb.AddForce(new Vector2(0, -rb.velocity.y));
-				rb.AddForce(new Vector2 (0, jumpForce));
-				candoublejump = true;
-			} else {
-				if (candoublejump) {
-					candoublejump = false;
-					rb.AddForce(new Vector2(0, -rb.velocity.y));
-					rb.AddForce (new Vector2 (0, jumpForce));
-				}
-			}
+	void FixedUpdate(){
+		if (knockBack <= 0) {
+			Move (Input.GetAxis ("Horizontal"));//isso passa como parametros os eixos horizontais. É necessário mecher nos axis do Unity e retirar o "a" e o "d" como gatilhos secudarios de horizontals
+			Jump ();
+		} else {
+			knockBack -= 1 * Time.deltaTime;
+		}
+		if (invencibilityTime > 0) {
+			invencibilityTime -= 1 * Time.deltaTime;
 		}
 	}
 
-	void GameOver() {
-		//vidas--;
-		RestartGame();
+	void GameOver(){
+		SceneManager.LoadScene ("prototipo",LoadSceneMode.Single);
 	}
 
-	void RestartGame (){
-		//vidas = 1;//por enquanto vidas = 1, pode mudar depois
-		Application.LoadLevel (0);
+	void Jump(){//esse código controla o pulo por um sistema dinâmico de contagem simples. O valor de jumps é zerado no código foot
+		if(Input.GetKeyDown(jump) && jumps < 1){//aqui o máximo de pulos está apenas como 1 pois, por algum motivo, o valor de jumps não implementa no primeiro pulo
+			rb.AddForce(new Vector2(rb.velocity.x, 0));
+			rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
+			jumps++;
+		}
 	}
 
-	void OnCollisionEnter2D(Collision2D coll2d) {
-		if(coll2d.gameObject.name == "colisor_queda") {
-			GameOver();
+	/*public IEnumerator Invencibility(){
+		
+	}*/
+
+	void PowerUp (Power power){
+		switch (power) {
+		case Power.powerUp1:
+			break;
+		case Power.powerUp2:
+			break;
+		case Power.powerUp3:
+			break;
+		default:
+			break;
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D other){
+		if (other.gameObject.tag == "PowerBox") {//esse bloco destroy o powerblock e pega o poder contido nela
+			Destroy (other.gameObject);
+			PowerBlock _power = other.gameObject.GetComponent<PowerBlock> ();
+			//effect = _power.power;
+			PowerUp(_power.power);
+		} 
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.tag == "PowerBox") {//esse bloco faz a mesma coisa que o superior, apenas foi acrescentado para deixar o gameplay mais flúido
+			Destroy (other.gameObject);
+			PowerBlock _power = other.gameObject.GetComponent<PowerBlock> ();
+			//effect = _power.power;
+			PowerUp(_power.power);
+		} 
+	}
+
+	void OnTriggerStay2D (Collider2D other){
+		/*if (other.gameObject.tag == "Enemy" && invencibilityTime <= 0 && this.gameObject.name == "Character") {//esse bloco faz o personagem recuar se ele levar um ataque
+			knockBack = 0.75f;
+			invencibilityTime = 1.5f;
+			rb.velocity = new Vector2 (0, 0);
+			rb.AddForce (new Vector2 (Mathf.Sign (transform.position.x - other.transform.position.x) * 5, 2), ForceMode2D.Impulse);
+		} else*/ if (other.gameObject.tag == "Insta Kill") {//esse bloco recarrega a fase caso o protagonista encoste em algum objeto que o mate instantaneamente
+			GameOver ();
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+		/*if (other.gameObject.tag == "Enemy" && invencibilityTime <= 0 && transform.parent == null) {//esse bloco faz a mesma coisa que o superior, apenas foi adicionadopara deixar o gameplay mais flúido
+			knockBack = 0.75f;
+			invencibilityTime = 1.5f;
+			rb.velocity = new Vector2 (0, 0);
+			rb.AddForce (new Vector2 (Mathf.Sign (transform.position.x - other.transform.position.x) * 5, 2), ForceMode2D.Impulse);
+		}else*/ if (other.gameObject.tag == "Insta Kill") {//esse bloco faz a mesma coisa que o superior, apenas foi adicionadopara deixar o gameplay mais flúido
+			GameOver ();
 		}
 	}
 
 	/*It was too glowing to be called a sword. 
 	 * Magenta, shining, heavy, and far too 80's. 
-	 * Indeed, it was a heap of raw motherfucking vapor-hazard.
-	*/
+	 * Indeed, it was a heap of raw motherfucking neon vapor-hazard.
+    */
 }
---
+++ Assets/Scripts/PowerBlock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerBlock : MonoBehaviour {
+
+	public Rigidbody2D rb;//passar o power block como parâmetro no inspector
+	public Collider2D cd;//passar o power block como parâmetro no inspector
+	public Power power;
+	float timer;
+	float initialTime;
+
+	void Start(){
+		power = (Power)Random.Range (1, 3);//isso faz com que o power block armazene um poder especial aleatório entre 2 e 3
+		rb.AddForce (new Vector2 (Random.Range (-100, 100), 350));//isso faz com que o power block "salte" para uma direção aleatória quando for instanciado
+		initialTime = Time.time;
+	}
+
+	void Update(){
+		timer = Time.time - initialTime;
+		if (timer >= 5.0f) {//esse bloco destroi o power block caso se passe mais de 5 segundos
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D (Collider2D other){//esse bloco faz com que power block fique intangível enquanto estiver no ar
+		if (other.tag == "Ground") {
+			cd.isTrigger = false;
+		}
+	}
+}
